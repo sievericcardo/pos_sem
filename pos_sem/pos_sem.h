e34926cfd8f72391c2c4f71e265093e525f56752
Riccardo Sieve <sievericcardo@gmail.com>
2018-01-26 20:35:28 +0100

    pos_sem/pos_sem.c: code for semaphores

---
+++ pos_sem/pos_sem.h
@@ -0,0 +1,14 @@
+//
+//  pos_sem.h
+//  pos_sem
+//
+//  Created by Riccardo SIeve on 25/01/2018.
+//  Copyright © 2018 Riccardo SIeve. All rights reserved.
+//
+
+#ifndef pos_sem_h
+#define pos_sem_h
+
+#include <stdio.h>
+
+#endif /* pos_sem_h */
